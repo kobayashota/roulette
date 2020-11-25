a1bbd307d65e4236b7330069dc9567fd8a2de9b7
kobashou06 <kobashou06@gmail.com>
2020-11-25 17:34:53 +0900

    アプリ完成

--- Swift5Timer1/ViewController.swift
+++ Swift5Timer1/ViewController.swift
@@ -18,8 +18,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        stopButton.isEnabled = true
+        
+        count = 0
+        
+        //stopButtonを押せなくする
+        stopButton.isEnabled = false
+        
+        for i in 0..<5{
+            
+            let image = UIImage(named: "\(i)")
+            imageArray.append(image!)
+        }
+        
+        imageView.image = UIImage(named: "0")
     }
     
     func startTimer(){
@@ -30,27 +41,32 @@ class ViewController: UIViewController {
                                      selector: #selector(timerUpdate),
                                      userInfo: nil,
                                      repeats: true)
-                                     
-        
+
     }
     
     //０.２秒ごとに呼ばれる
     @objc func timerUpdate(){
         
         count = count + 1
+        
+        if count > 4 {
+            count = 0
+        }
+        
+        print(count)
+        
         imageView.image = imageArray[count]
-        
-        
         
     }
     
 
     @IBAction func start(_ sender: Any) {
-        
         //imageViewのimageに画像を反映していく
         
+        startTimer()
         //startButtonは押せなくする
-        startButton.isEnabled = true
+        startButton.isEnabled = false
+        stopButton.isEnabled = true
         
     }
     
@@ -59,8 +75,11 @@ class ViewController: UIViewController {
         //imageViewのimageに反映されている画像の流れをストップする
         
         //startButtonを押せるようにする
-        startButton.isEnabled = false
+        startButton.isEnabled = true
+        stopButton.isEnabled = false
         
+        //タイマーを止める
+        timer.invalidate()
     }
     
     
